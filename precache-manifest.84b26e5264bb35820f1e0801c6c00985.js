b29d71f21f766cf0ceb699b3179e7e39a7ff00cb
Yogesh Raj Kabilan <rajyogesh46@gmail.com>
2020-05-25 13:46:32 +0530

    Updates

---
+++ precache-manifest.84b26e5264bb35820f1e0801c6c00985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e13261203aac36966655e355af2e49",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d42c79854f89f5162fcb",
+    "url": "/react-todo/static/css/main.b0c7c06c.chunk.css"
+  },
+  {
+    "revision": "f722c8e3d553e46dd1b2",
+    "url": "/react-todo/static/js/2.8327e78c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo/static/js/2.8327e78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42c79854f89f5162fcb",
+    "url": "/react-todo/static/js/main.df2f86cb.chunk.js"
+  },
+  {
+    "revision": "5190324ecb34ab2d5bd4",
+    "url": "/react-todo/static/js/runtime-main.0dd0b0c2.js"
+  }
+]);
